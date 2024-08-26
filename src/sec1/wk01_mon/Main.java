5623a392ed444ac1a1a1a2014daf2de636749b49
sislam <sislam@csumb.edu>
2024-08-26 12:19:20 -0700

    Wk01 Monday Class

--- src/sec1/wk01_mon/Main.java
+++ src/sec1/wk01_mon/Main.java
@@ -1,7 +1,39 @@
 package sec1.wk01_mon;
 
+import java.util.Random;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String [] args){
-        System.out.println("Hello");
+        Random rand = new Random();
+
+        int max = 10;
+        int secret = Math.abs(rand.nextInt()) % max + 1;
+//        System.out.println("Rand: " + secret);
+
+        int count = 0;
+        int limit = 4;
+        int [] guesses = new int[limit];
+
+        Scanner scan = new Scanner(System.in);
+        do{
+            System.out.print("Enter a guess from 1 to " + max + ": ");
+            int guess = scan.nextInt();
+            guesses[count] = guess;
+            count++;
+            if(guess == secret){
+                System.out.println("You guessed correctly after " + count + " attempt(s)!");
+                break;
+            }
+            else {
+                System.out.println("Your guess is wrong! Try Again!");
+                System.out.println("Your have " + (limit - count) + " guess(es) left");
+            }
+        }while(count < limit);
+        System.out.println("The secret number is: " + secret);
+        System.out.println("Your Guesses: ");
+        for(int i = 0; i < count; i++){
+            System.out.println(guesses[i]);
+        }
     }
 }
--- src/sec2/wk01_mon/Main.java
+++ src/sec2/wk01_mon/Main.java
@@ -2,6 +2,7 @@ package sec2.wk01_mon;
 
 public class Main {
     public static void main(String [] args){
+        int x = 5;
         System.out.println("Hello");
     }
 }
