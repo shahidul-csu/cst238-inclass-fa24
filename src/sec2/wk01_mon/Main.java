2987908ada318e1d6306f0e0e1f1b3a9f0848a72
sislam <sislam@csumb.edu>
2024-08-26 18:28:20 -0700

    Monday Class Code

--- src/sec2/wk01_mon/Main.java
+++ src/sec2/wk01_mon/Main.java
@@ -1,7 +1,35 @@
 package sec2.wk01_mon;
 
+import java.util.Random;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String [] args){
-        System.out.println("Hello");
+        Random rand = new Random();
+        Scanner in = new Scanner(System.in);
+        int range = 10;
+        int limit = 4;
+        int count = 0;
+        int [] guesses = new int[limit];
+        int secret = Math.abs(rand.nextInt()) % range + 1;
+        do{
+            System.out.print("Enter and guess in 1 to " + range + ": ");
+            int guess = in.nextInt();
+            guesses[count] = guess;
+            count++;
+            if(guess == secret){
+                System.out.println("Your guess is correct!");
+                break;
+            } else{
+                System.out.println("Your guess is incorrect! Try again!");
+                System.out.println("Your have " + (limit - count) + " guess(es) left!");
+            }
+        }while(count < limit);
+        System.out.println("Secret: " + secret);
+        System.out.println("Your Guesses: ");
+        for(int i = 0; i < count; i++){
+            System.out.println(guesses[i]);
+        }
+//        System.out.println("Hello");
     }
 }
