472d5ccbc56fbecfab9e506e96d377e26bfcb1b5
sislam <sislam@csumb.edu>
2024-11-18 17:14:53 -0800

    Wk13 Monday Class Code

---
+++ src/sec2/wk13_mon/BSTRunner.java
@@ -0,0 +1,14 @@
+package sec2.wk13_mon;
+
+public class BSTRunner {
+    public static void main(String[] args){
+        BST b1 = new BST();
+        b1.add(7);
+        b1.add(5);
+        b1.add(9);
+        b1.add(6);
+        b1.add(4);
+        b1.add(8);
+        b1.preOrder();
+    }
+}
